3edcf1c0452f8cc6c770e5c2111cb91d3d8f52ef
seanaus <94388567+seanaus@users.noreply.github.com>
2022-03-15 21:23:12 +0000

    filters tweeks

--- site/js/filters.js
+++ site/js/filters.js
@@ -1,5 +1,6 @@
 
 document.addEventListener('DOMContentLoaded', (event) => {
+    console.log("CALL INIT");
     initialize();
 });
 const parentProductLevel = (productLevel) => {
@@ -16,8 +17,8 @@ const parentProductLevel = (productLevel) => {
 // 13/01/2022
 // ...
 initialize = () => {
-
-    const filterState = JSON.parse(localStorage.getItem("filterState"));
+    console.log("INIT");
+    // const filterState = JSON.parse(localStorage.getItem("filterState"));
     const btns = document.querySelectorAll("div[data-node-type='child']");
     btns.forEach((btn) => {
         if (btn) {
@@ -42,6 +43,7 @@ initialize = () => {
 // 23/03/2022
 // 
 const filterAction = (event) => {
+    console.log("FILTER ACTION");
     // data-node-type = "parent || child"
     // data-node-level = "1,2,3,4"
     // data-togglable = "true || false"
@@ -49,10 +51,13 @@ const filterAction = (event) => {
     // data-productGroupId = "'none' || productGroupId"
     const btn = event.target;
     const level = btn.attributes["data-node-level"].value;
-    const _parentProductLevel = parentProductLevel(btn.attributes["data-productLevel"].value);
     const productLevel = btn.attributes["data-productLevel"].value;
-    
-    console.log(`productLevel is ${productLevel}`);
+    const _parentProductLevel = parentProductLevel(productLevel);
+
+    console.log(`level: ${level}`);
+    console.log(`productLevel: ${productLevel}`);
+    console.log(`_parentProductLevel: ${_parentProductLevel}`);
+
     clearSubFilters(level);
     clearIsSelected(level);
 
@@ -62,8 +67,8 @@ const filterAction = (event) => {
     // };
     // localStorage.setItem("filterState", JSON.stringify(filterState));
     // window.location.href = `../products?productLevel=${productLevel}`;
-    showSubFilters(level, productLevel,"DOWN");
-    // addClass(btn, "isSelected");
+    showSubFilters(level, productLevel);
+    addClass(btn, "isSelected");
     
 };
 const clearSubFilters = (level) => {
@@ -74,39 +79,20 @@ const clearSubFilters = (level) => {
         }
     });
 };
-const saveFilterState = () => {
-    const btns = document.querySelectorAll("div[data-node-type ='parent']");
-    btns.forEach((btn) => {
-    });
-};
-const showSubFilters = (level, productLevel, direction = "DOWN") => {
-    console.log("showSubFilters");
-        console.log(level);
-        console.log(productLevel);
-
+// const saveFilterState = () => {
+//     const btns = document.querySelectorAll("div[data-node-type ='parent']");
+//     btns.forEach((btn) => {
+//     });
+// };
+const showSubFilters = (level,productLevel) => {
     if (level != "-1" && productLevel != "-1") {
-        console.log(level);
-        console.log(productLevel);
-        console.log("DIR " + direction);
         const btns = document.querySelectorAll("div[data-node-type ='parent']");
         btns.forEach((btn) => {
-            if (direction == "DOWN") {
-                console.log(direction);
-                if (parseInt(btn.attributes["data-node-level"].value) == parseInt(level) + 1 && btn.attributes["data-togglable"].value == 'true' && btn.attributes["data-productLevel"].value == productLevel) {
-                    removeClass(btn, "hideMe")
-                }
-            } else {
-                if (parseInt(btn.attributes["data-node-level"].value) <= parseInt(level) && btn.attributes["data-togglable"].value == 'true' && btn.attributes["data-productLevel"].value == productLevel) {
-                    removeClass(btn, "hideMe");
-                    console.log(direction);
-                }
-                if (parseInt(btn.attributes["data-node-level"].value) == parseInt(level) + 1 && btn.attributes["data-togglable"].value == 'true' && btn.attributes["data-productLevel"].value == productLevel) {
-                    removeClass(btn, "hideMe")
-                }
+            if (parseInt(btn.attributes["data-node-level"].value) == parseInt(level) + 1 && btn.attributes["data-togglable"].value == 'true' && btn.attributes["data-productLevel"].value == productLevel) {
+                removeClass(btn, "hideMe")
             }
         });  
     }
-
 };
 const clearIsSelected = (level)=> {
     const btns = document.querySelectorAll("div[data-node-type='child']");
