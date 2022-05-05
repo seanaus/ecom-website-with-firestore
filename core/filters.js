564c623c23a9735bb14bf22199ad56af4a949d44
seanaus <94388567+seanaus@users.noreply.github.com>
2022-05-05 19:37:55 +0100

    parent filter bars now load dynamically

--- core/filters.js
+++ core/filters.js
@@ -5,6 +5,7 @@ const { loadConfig } = require("../core/config");
 const FilterItem = require("../models/FilterItem");
 
 let nodes = [];
+let levels = [];
 
 const IsArray = (aryCheck) => {
   return aryCheck && Array.isArray(aryCheck) && aryCheck.length > 0;
@@ -47,69 +48,67 @@ const parse = (node) => {
 // };
 // const loadFiltersOld = async () => {
 //   let nodes = [];
-  // try {
-  //   let nodes = [];
-  //   const filters = await loadConfig("filters");
-  //   filters.items.forEach((item, idx) => {
-  //     const node = new FilterItem(
-  //       item.id,
-  //       item.level,
-  //       item.name,
-  //       item.parentId,
-        // item.children = parse(item)
-      // );
-    //   console.log("NODE");
-    //   console.log(JSON.stringify(node));
-      // nodes.push(node);
-  //   });
-  //   return nodes
-  // } catch (error) {
-  //   console.log(error.message);
-  //   return []
-  // }
+// try {
+//   let nodes = [];
+//   const filters = await loadConfig("filters");
+//   filters.items.forEach((item, idx) => {
+//     const node = new FilterItem(
+//       item.id,
+//       item.level,
+//       item.name,
+//       item.parentId,
+// item.children = parse(item)
+// );
+//   console.log("NODE");
+//   console.log(JSON.stringify(node));
+// nodes.push(node);
+//   });
+//   return nodes
+// } catch (error) {
+//   console.log(error.message);
+//   return []
+// }
 //   console.log("NODES");
 //   console.log(JSON.stringify(nodes));
 //   return nodes;
 // };
 
-
 const loadFilters = async () => {
-    try {
-        nodes = [];
-        const data = await firestore.collection("components")
-        const docs = await data.get();
-        if (docs.empty) {
-            console.log("ERROR: No filter data found");
-        } else {
-            console.log("FILTERS");
-            docs.forEach((doc) => {
-              if (doc.id === "filters") {
-                doc.data().items.forEach((item) => {
-                  const node = new FilterItem(
-                      item.id,
-                      item.level,
-                      item.name,
-                      item.parentId
-                      // item.children = parse(item)
-                  );
-                  nodes.push(node);
-                })
-              }
-            });
-            nodes.forEach((node)=> {
-              const children = getChildren(node);
-              children.forEach((child)=> {
-                node.children.push(child)
-              })
-            })
-            console.log(nodes);
-            return nodes;
+  try {
+    nodes = [];
+    levels = [];
+    const data = await firestore.collection("components");
+    const docs = await data.get();
+    if (docs.empty) {
+      console.log("ERROR: No filter data found");
+    } else {
+      docs.forEach((doc) => {
+        if (doc.id === "filters") {
+          doc.data().items.forEach((item) => {
+            const node = new FilterItem(
+              item.id,
+              item.level,
+              item.name,
+              item.parentId
+            );
+            nodes.push(node);
+          });
         }
-    } catch (error) {
-        console.log(error.message);
-        return [];
+      });
+      nodes.forEach((node) => {
+        const children = getChildren(node);
+        children.forEach((child) => {
+          node.children.push(child);
+        });
+      });
+      // console.log(nodes);
+      return nodes;
     }
+  } catch (error) {
+    console.log(error.message);
+    return [];
+  }
 };
 module.exports = {
-  loadFilters
+  loadFilters,
 };
--- site/js/filters.js
+++ site/js/filters.js
@@ -1,4 +1,47 @@
+window.onload = () => {
+    const count = localStorage.getItem("filterLevels");
+    addFilterRows(count);
+}
+const addFilterRows = (count) => {
+  count = count == 0 ? 1 : count;
+  for (let level = 1; level <= count; level++) {
+    addHTMLElement("filterPanel", "div", level);
+  }
+};
+const addHTMLElement = (
+  parentId = "body",
+  type = "div",
+  id = "",
+  value = "",
+  nodeType = "parent") => {
+  //parentId - text value used to select the parent element
+  //type - text value stating the type of HTML element to be added
+  //value - text value, this will be visible on screen
 
+  // Get Elements by id of the form inputs
+  let parentElement = document.getElementById(parentId);
+  let newElement = "";
+
+  // createElement() method is used
+  // to create a new element
+  newElement = document.createElement(type);
+
+  // Append a text node for this example
+  newElement.appendChild(document.createTextNode(value));
+
+  // setAttribute() is used to set
+  // the attributes of the element
+  newElement.setAttribute("data-node-type", `${nodeType}`);
+  if (id != "") {
+    newElement.setAttribute("id", `${id}`);
+    newElement.setAttribute("data-level", `${id}`);
+  }
+  newElement.setAttribute("class", "filterRow");
+
+  // Append as child to the parent
+  // i.e. body
+  parentElement.appendChild(newElement);
+};
 // document.addEventListener('DOMContentLoaded', (event) => {
 //     console.log("CALL INIT");
 //     initialize();
@@ -25,57 +68,57 @@
 //             btn.addEventListener("click", filterAction);
 //         }
 //     });
-    
-    // if (filterState) {
-    //     const level = filterState.level ? -1 : filterState.level;
-    //     const productLevel = filterState.productLevel == undefined ? -1 : filterState.productLevel;
-    //     // alert(`level = ${leve
-    //     showSubFilters(level, productLevel, "UP");
-    //     showSubFilters(level, productLevel,"DOWN");
-    // } else {
-    //     console.log("FFS");
-    // }
 
-    // addClass(btn, "isSelected");
+// if (filterState) {
+//     const level = filterState.level ? -1 : filterState.level;
+//     const productLevel = filterState.productLevel == undefined ? -1 : filterState.productLevel;
+//     // alert(`level = ${leve
+//     showSubFilters(level, productLevel, "UP");
+//     showSubFilters(level, productLevel,"DOWN");
+// } else {
+//     console.log("FFS");
+// }
+
+// addClass(btn, "isSelected");
 // };
 
 // Sean Austin
 // 23/03/2022
-// 
+//
 // const filterAction = (event) => {
 //     console.log("FILTER ACTION");
-    // data-node-type = "parent || child"
-    // data-node-level = "1,2,3,4"
-    // data-togglable = "true || false"
-    // data-productLevel = "productLevel01,productLevel02" 
-    // data-productGroupId = "'none' || productGroupId"
-    // const btn = event.target;
-    // const level = btn.attributes["data-node-level"].value;
-    // const productLevel = btn.attributes["data-productLevel"].value;
-    // const _parentProductLevel = parentProductLevel(productLevel);
+// data-node-type = "parent || child"
+// data-node-level = "1,2,3,4"
+// data-togglable = "true || false"
+// data-productLevel = "productLevel01,productLevel02"
+// data-productGroupId = "'none' || productGroupId"
+// const btn = event.target;
+// const level = btn.attributes["data-node-level"].value;
+// const productLevel = btn.attributes["data-productLevel"].value;
+// const _parentProductLevel = parentProductLevel(productLevel);
 
-    // console.log(`level: ${level}`);
-    // console.log(`productLevel: ${productLevel}`);
-    // console.log(`_parentProductLevel: ${_parentProductLevel}`);
+// console.log(`level: ${level}`);
+// console.log(`productLevel: ${productLevel}`);
+// console.log(`_parentProductLevel: ${_parentProductLevel}`);
 
-    // clearSubFilters(level);
-    // clearIsSelected(level);
+// clearSubFilters(level);
+// clearIsSelected(level);
 
-    // const filterState = {
-    //     level: level,
-    //     productLevel: _parentProductLevel
-    // };
-    // localStorage.setItem("filterState", JSON.stringify(filterState));
-    // window.location.href = `../products?productLevel=${productLevel}`;
-    // showSubFilters(level, productLevel);
-    // addClass(btn, "isSelected");
-    
+// const filterState = {
+//     level: level,
+//     productLevel: _parentProductLevel
+// };
+// localStorage.setItem("filterState", JSON.stringify(filterState));
+// window.location.href = `../products?productLevel=${productLevel}`;
+// showSubFilters(level, productLevel);
+// addClass(btn, "isSelected");
+
 // };
 // const clearSubFilters = (level) => {
 //     const btns = document.querySelectorAll("div[data-node-type ='parent']");
 //     btns.forEach((btn) => {
 //         if (parseInt(btn.attributes["data-node-level"].value) > parseInt(level) && btn.attributes["data-togglable"]) {
-//             addClass(btn,"hideMe")     
+//             addClass(btn,"hideMe")
 //         }
 //     });
 // };
@@ -91,14 +134,14 @@
 //             if (parseInt(btn.attributes["data-node-level"].value) == parseInt(level) + 1 && btn.attributes["data-togglable"].value == 'true' && btn.attributes["data-productLevel"].value == productLevel) {
 //                 removeClass(btn, "hideMe")
 //             }
-//         });  
+//         });
 //     }
 // };
 // const clearIsSelected = (level)=> {
 //     const btns = document.querySelectorAll("div[data-node-type='child']");
 //     btns.forEach((btn)=>{
 //         if (parseInt(btn.attributes["data-node-level"].value) >= parseInt(level)) {
-//             removeClass(btn,"isSelected")     
+//             removeClass(btn,"isSelected")
 //         }
 //     });
 // }
@@ -114,12 +157,12 @@
 // };
 // const applyFilter = (level) => {
 //     if (parseInt(level)===1) {
-        
+
 //     }
 //     if (parseInt(level)===2) {
-        
+
 //     }
 //     if (parseInt(level)===3) {
-        
+
 //     }
 // };
