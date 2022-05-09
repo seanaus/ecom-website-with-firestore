8f04b70a64261e40215dfb4fa52983528d576b2e
seanaus <94388567+seanaus@users.noreply.github.com>
2022-05-09 17:31:19 +0100

    filter mods

--- site/js/filters.js
+++ site/js/filters.js
@@ -1,50 +1,103 @@
 window.onload = () => {
+  initialize();
+}
+const initialize = () => {
+  console.log("INIT");
+  // const filterState = JSON.parse(localStorage.getItem("filterState"));
+  const btns = document.querySelectorAll("div[data-node-type='CHILD']");
+  btns.forEach((btn) => {
+    if (btn) {
+      btn.addEventListener("click", filterAction);
+    }
+  });
+} 
+const filterAction = (event) => {
+  const btn = event.target
+  const nodeId = btn.attributes["data-node-id"].value;
+  const nodeLevel = btn.attributes["data-node-level"].value;
+  const subLevel = parseInt(nodeLevel) + 1;
+  const nodeName = btn.attributes["data-node-name"].value;
+  const nodeParentId = btn.attributes["data-node-parentId"].value;
+  const nodeChildren = JSON.parse(btn.attributes["data-node-children"].value);
+  const nodeType = btn.attributes["data-node-type"].value;
+  
+  const reset = document.querySelectorAll("div[data-node-type='CHILD']");
+
+  // TRY TO SELECT SUB LEVEL PARENT
+  const parent = document.getElementById(`P${subLevel}`);
+  // CHECK FOR SUB NODES
+  const hasSubLevel = parent.id ? true : false
+  if (hasSubLevel) {
+    // SHOW PARENT
+    removeClass(parent, "hideMe")
+    // SHOW CHILDREN RELATED TO THE PARENT
+    const selector = `div[data-node-level='${subLevel}'] div[data-node-parentId='${nodeId}']`
+    console.log(selector)
+    const subItems = document.querySelectorAll(selector);
+    subItems.forEach((subItem) => { 
+      removeClass(subItem,"hideMe")
+    })
+    console.log(subItems);
+  }
+
+  
+  // console.log(`id: ${nodeId}`);
+  // console.log(`level: ${nodeLevel}`);
+  // console.log(`name: ${nodeName}`);
+  // console.log(`parent: ${nodeParentId}`);
+  // console.log(`children: ${nodeChildren}`);
+  // console.log(`nodeType: ${nodeType}`);
+  
+
+}
+
+// window.onload = () => {
     // const count = localStorage.getItem("filterLevels");
-  const count = "<%= filters.levels.length %>"
-  addFilterRows(count);
+  // const count = "<%= filters.levels.length %>"
+  // addFilterRows(count);
   // const items = "<%= filters.items %>"
   // console.log(JSON.stringify(items));
-}
-const addFilterRows = (count) => {
-  count = count == 0 ? 1 : count;
-  for (let level = 1; level <= count; level++) {
-    addHTMLElement("filterPanel", "div", level);
-  }
-};
-const addHTMLElement = (
-  parentId = "body",
-  type = "div",
-  id = "",
-  value = "",
-  nodeType = "parent") => {
+// }
+// const addFilterRows = (count) => {
+//   count = count == 0 ? 1 : count;
+//   for (let level = 1; level <= count; level++) {
+//     addHTMLElement("filterPanel", "div", level);
+//   }
+// };
+// const addHTMLElement = (
+//   parentId = "body",
+//   type = "div",
+//   id = "",
+//   value = "",
+//   nodeType = "parent") => {
   //parentId - text value used to select the parent element
   //type - text value stating the type of HTML element to be added
   //value - text value, this will be visible on screen
 
   // Get Elements by id of the form inputs
-  let parentElement = document.getElementById(parentId);
-  let newElement = "";
+  // let parentElement = document.getElementById(parentId);
+  // let newElement = "";
 
   // createElement() method is used
   // to create a new element
-  newElement = document.createElement(type);
+  // newElement = document.createElement(type);
 
   // Append a text node for this example
-  newElement.appendChild(document.createTextNode(value));
+  // newElement.appendChild(document.createTextNode(value));
 
   // setAttribute() is used to set
   // the attributes of the element
-  newElement.setAttribute("data-node-type", `${nodeType}`);
-  if (id != "") {
-    newElement.setAttribute("id", `${id}`);
-    newElement.setAttribute("data-level", `${id}`);
-  }
-  newElement.setAttribute("class", "filterRow");
+  // newElement.setAttribute("data-node-type", `${nodeType}`);
+  // if (id != "") {
+  //   newElement.setAttribute("id", `${id}`);
+  //   newElement.setAttribute("data-level", `${id}`);
+  // }
+  // newElement.setAttribute("class", "filterRow");
 
   // Append as child to the parent
   // i.e. body
-  parentElement.appendChild(newElement);
-};
+//   parentElement.appendChild(newElement);
+// };
 // document.addEventListener('DOMContentLoaded', (event) => {
 //     console.log("CALL INIT");
 //     initialize();
@@ -148,16 +201,16 @@ const addHTMLElement = (
 //         }
 //     });
 // }
-// const removeClass = (element, className) => {
-//   if (element) {
-//     element.classList.remove(className);
-//   }
-// };
-// const addClass = (element, className) => {
-//   if (element) {
-//     element.classList.add(className);
-//   }
-// };
+const removeClass = (element, className) => {
+  if (element) {
+    element.classList.remove(className);
+  }
+};
+const addClass = (element, className) => {
+  if (element) {
+    element.classList.add(className);
+  }
+};
 // const applyFilter = (level) => {
 //     if (parseInt(level)===1) {
 
